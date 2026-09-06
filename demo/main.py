cc0a20e3c4a05d5b050677189d0f4de999e9e038
catos@misser.top <missercatos@users.noreply.github.com>
2026-09-06 23:16:53 +0800

    建立demo基础连接系统

--- demo/main.py
+++ demo/main.py
@@ -1,10 +1,21 @@
-import os
-
-os.environ["MAAFW_BINARY_PATH"] = os.path.expanduser("~/maa-bin")
-
-from maa.resource import Resource
+from maa import controller
+from maa.controller import AdbController
 from maa.toolkit import Toolkit
 
+# 初始化
 Toolkit.init_option("./")
-resource = Resource()
-resource.post_bundle("./resource").wait()
+
+# 查找设备
+devices = Toolkit.find_adb_devices()
+if not devices:
+    exit(1)
+devices = devices[0]
+
+# 创建连接
+controller = AdbController(
+    adb_path=devices.adb_path,
+    address=devices.adb_path,
+    screencap_methods=devices.screencap_methods,
+    input_methods=devices.input_methods,
+    config=devices.config,
+)
